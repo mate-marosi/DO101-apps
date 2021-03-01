fcca3a33a77b094b1a3109618aa4317d672a5ab3
Mate Marosi <mate.marosi@accenture.com>
2021-03-01 23:21:14 +0100

    jo éjszakát

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello Dorottya!\n');
+  res.send('Jó éjszakát!\n');
 });
 
 app.listen(8080, function () {
